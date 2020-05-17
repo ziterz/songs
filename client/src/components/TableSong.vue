e83ccb35e6928d347ada7a4d73bc1dc8437cec94
IrwanKhaliq <Irwan.khaliq10@gmail.com>
2020-05-17 19:22:08 +0700

    last commit

--- client/src/components/TableSong.vue
+++ client/src/components/TableSong.vue
@@ -2,11 +2,10 @@
   <div>
     <h1>Playlist</h1><br>
     <b-button variant="outline-primary" @click="add">Add New Playlist</b-button><br><br>
-    <b-table striped hover :items="items">
-            <template v-slot:cell(actions)="data">
-        <span v-html="data.value"></span>
-      </template>
-    </b-table>
+    <b-input-group prepend="delete" @click="deleted" class="mb-2 mr-sm-2 mb-sm-0">
+      <b-input id="inline-form-input-username" placeholder="give me id of list, then click button on left side" v-model="id"></b-input>
+    </b-input-group><br><br>
+    <b-table striped hover :items="items"></b-table>
   </div>
 </template>
 
@@ -16,28 +15,41 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      items: []
+      items: [],
+      id: ''
     }
   },
   methods: {
-    add () { this.$router.push({ path: '/add-playlist' }) }
-  },
-  created () {
-    if (localStorage.access_token) {
+    add () { this.$router.push({ path: '/add-playlist' }) },
+    deleted () {
+      axios({
+        method: 'DELETE',
+        url: `http://localhost:3000/songs/${this.id}`,
+        headers: { access_token: localStorage.access_token }
+      })
+        .then(deleted => {
+          this.load()
+          this.id = ''
+        })
+        .catch(err => { console.log(err) })
+    },
+    load () {
       axios({
         method: 'GET',
         url: 'http://localhost:3000/songs',
         headers: { access_token: localStorage.access_token }
       })
         .then(({ data }) => {
-          data.songs.map(el => {
-            el.actions = '<b-button variant="outline-danger" @click="deleteSong"><span v-html="data.value"></span></b-button>'
-          })
           this.items = data.songs
         })
         .catch(err => {
           console.log(err)
         })
+    }
+  },
+  created () {
+    if (localStorage.access_token) {
+      this.load()
     } else {
       this.$router.push({ path: '/' })
     }
